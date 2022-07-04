482dd7e52a66a169aa9902c560b44069d0b58675
Blwx <758105389@qq.com>
2022-07-04 22:39:56 +0800

    keep secret
    keep secret

--- GetJsonUtil2.java
+++ GetJsonUtil2.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
  * 传入一个javaBean，为它所有的字段都赋好合适的值，并返回。
  * 注意：这里为了解耦，返回的任然是一个对象，而不是json，这样子方便调用者的处理。
  *
- * @author hwx1031036
+ * @author **********(secret)
  * @since 2021-03-12
  */
 public class GetJsonUtil {
